21b688f4b651e71ce0182d29918192f422821d82
Firdaws <firdaws.ab.h@gmail.com>
2023-08-14 23:45:47 +0300

    Here ,I talk about commenting very easily , with example

---
+++ Comment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            //   This type is Single line comment.
+
+            //   If you want to send a message to your fellow programmer, you can use comments.
+
+
+            /* 
+                 This type is name Delimited comment.
+                 Actions or text written in comments are ignored by the compiler,
+                 Therefore, nothing appears in this file at runtime.
+             */
+
+
+            /**
+             * 
+             *    This type is name Single-line delimiter.
+             *    If you want to remember how a code works. You can write a comment for it.
+             * 
+             **/
+
+
+            ///<summary>
+            ///   
+            /// 
+            ///   This type is name Delimited Comment.
+            ///   If you want to talk about a code and comment on it, you can write a comment.
+            ///   You are free to write any kind of comment.
+            /// 
+            /// 
+            /// 
+            /// </summary>
+
+
+
+            // An example of comment
+
+            int X = 10;
+            Console.WriteLine(X);   // here X= 10
+
+        }
+
+
+
+    }
+}
