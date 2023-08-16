b639b6924f0f33c79f6defa80cf4433f4d079fee
Firdaws <firdaws.ab.h@gmail.com>
2023-08-16 09:51:33 +0300

    Here is a starting point for the types and how they are stored

---
+++ Type_Storage.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            /* 
+                  in the bigning of learn C# , you need to Know about type and 
+                  learning C# consists of learning how to create and use types
+                  some of the type is a simple type like (int , long, shurt...) can store only a single data item. 
+                  other type is can store multiply item like (Array)
+            */
+
+
+
+            /* 
+           
+                   Data type 
+                   We have three part of the Data type
+                 
+                 1- Reference Data Types : require two segments (part) of memory .
+                   The first contains the actual data and is always located in the heap.
+                   The second is a reference that points to where the data is stored in the heap.
+                   (For reference types, the actual data is stored in the heap, and the reference is stored on the stack)
+
+                 2- Value Data type : require only a single segment (part) of memory, which stores the actual data.
+                    (for value type Data is stored on the stack )
+
+                 3- Pointer Data Type: that points to an address of a value,
+                    We can say that the symbols used are Pointer Data Type such as & and *
+                    
+
+
+
+                   Stack and Heap 
+                   when an program is running, its data must be stored in memory ,where and how it’s stored?
+                   A running program uses two part of memory to store data: the stack and the heap.
+
+                   
+                   stack 
+                   Stack is a special type of collection that stores elements in LIFO (Last In First Out) 
+                   LIFO means The last item added can be removed first.
+                   And addition items is called push and deviation items is called pop.
+
+
+                   Heap 
+                   It is an area of memory allocated to store certain kinds of data objects.
+                   In it data can be stored and removed in any order.
+                   here Garbage Collection removes all items that do nothing or have no origin.
+
+
+
+
+               
+                   There are 2 types of "Value data type" in C# language
+                   Predefined Data Types and Users defined Data Types
+              
+                  
+                    Predifine data 
+                    __________________
+                   Numeric 
+                       1- Intiger
+                         ( sbyte ,byte ,short ,ushort ,int , uint ,long),
+                       2- Floting point( decimal,float,double)) 
+                   Non Numeric 
+                         (bool,char )
+
+                   
+                     Users difine data 
+                    ___________________
+                      Enum type
+                      Struct type
+                    
+                  
+
+
+ 
+                  There are 2 types of "Reference data type" in C# language
+                   Predefined Data Types and Users defined Data Types
+
+                   Predifine data 
+                    __________________
+                       Object 
+                       String 
+                       Daynamic
+
+                   
+                     Users difine data 
+                    ___________________
+                      Class
+                      Interface
+                      Delegate
+                     
+              */
+
+        }
+    }
+}
