cb5342bc95d3f4c1c8e3693cb2454625a6e5741d
Firdaws <firdaws.ab.h@gmail.com>
2023-08-19 17:53:44 +0300

    What are Statements ?

---
+++ Statement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Statements
+
+             Statements are the actions that your program performs,
+             from defining data to if conditions or any block of code,calling methods...
+            */
+
+            int x = 10;   // Diclaration Stetment
+
+            int y;        //Assigment Stetment
+            y = 20;
+            y++;           //Expression stetment
+
+
+            /* we have a lot of different type of statement like
+            if statement , switch statement , labeled statement .
+            we talk about each of statement in the files
+            */
+
+
+        }
+    }
+}
